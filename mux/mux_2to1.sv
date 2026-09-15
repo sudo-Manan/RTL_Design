650ea1d1bc6299726fb39b884e77acd9060aa222
sudo-Manan <called.manan.i.am@gmail.com>
2026-09-16 03:46:11 +0530

    feat: add mux

--- mux/mux_2to1.sv
+++ mux/mux_2to1.sv
@@ -1,1 +1,15 @@
-//use dataflow model
+// 
+// Module Name: mux2to1
+// Description: 2:1 mux design using dataflow model, no ternary operation
+// 
+// Dependencies: 
+// 
+
+module mux2to1 (
+    input logic in_d1, in_d0,
+    input logic in_sel,
+    output logic out_y
+);
+    assign out_y = (~in_sel & in_d0) | (in_sel & in_d1);
+
+endmodule
--- mux/mux_4to1.sv
+++ mux/mux_4to1.sv
@@ -1,1 +1,22 @@
-//use behavioural/rtl modelling
+// 
+// Module Name: mux4to1
+// Description: 4:1 mux design using behavioural model/ rtl model
+// 
+// Dependencies: 
+// 
+
+module mux_4to1 (
+    input logic in_d3, in_d2, in_d1, in_d0,
+    input logic [1:0] in_sel,
+    output logic out_y
+);
+    always_comb begin
+        case (in_sel)
+            2'b11: out_y = in_d3;
+            2'b10: out_y = in_d2;
+            2'b01: out_y = in_d1;
+            2'b00: out_y = in_d0;
+            default: out_y = 1'b0;
+        endcase
+    end
+endmodule
